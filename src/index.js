34f725a74ac5ed15d9eeba84c73cd5f8bd08ecf3
YasahiiSoftware <yasashiisoftware@gmail.com>
2019-11-08 19:23:40 -0800

    before h1 mod

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ ReactDOM.render(
     <h1 className="container text-center">React Prototypes!</h1>,
     document.getElementById('root')
 );
+
+function luckyNumber () {
+    return Math.random() * (1000 - 1) + 1;
+}
+
+function 
