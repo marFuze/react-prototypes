f5a23f6c8628ed90bdfc740becd5588708491e23
YasahiiSoftware <yasashiisoftware@gmail.com>
2019-11-08 20:26:13 -0800

    vars not displaying

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1 className="container text-center">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+const user = {
+    name: "Stuart",
+    luckyNumber: luckyNumber()
+};
 
 function luckyNumber () {
     return Math.random() * (1000 - 1) + 1;
 }
 
-function 
+function greeting (user) {
+    return <div className="container">
+        
+        <h1>`Hello {user.name}`</h1>
+        <h2 className="text-muted">Your lucky number is: {user}</h2>
+        
+    </div>
+}
+
+
