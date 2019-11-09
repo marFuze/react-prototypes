cabd34744de0125042a265fa1142961fe83ef97c
YasahiiSoftware <yasashiisoftware@gmail.com>
2019-11-08 20:50:06 -0800

    mid commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,23 @@
+import React from 'react';
+import Table from './table';
 
+function App () {
+    return <div className="container">
+        <h1>
+            Student Grade Table
+        </h1>
+        <Table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>Grade</th>
+                </tr>
+            </thead>
 
+        </Table>
+
+    </div>
+}
+
+export default App;
